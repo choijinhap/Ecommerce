e31e3c30769f15a4b3327c60a9538021dca6ba2c
choijinhap <wlsgkq123@gmail.com>
2022-04-05 22:49:21 +0900

    dev : categories

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+const categories = [
+	{ id: '1', title: 'Hats' },
+	{ id: '2', title: 'Jackets' },
+	{ id: '3', title: 'Sneakers' },
+	{ id: '4', title: 'Womens' },
+	{ id: '5', title: 'Mens' },
+];
 const App = () => {
 	return (
-		<div >
-			
+		<div className='categories-container'>
+			{categories.map(({ id, title }) => (
+				<div className='category-container'>
+					<div className='background-image'></div>
+					<div className='category-body-container' key={id}>
+						<h2>{title}</h2>
+						<p>Shop now</p>
+					</div>
+				</div>
+			))}
 		</div>
 	);
 };
