815128bbe160a8c199f22255afd131d12d40d2a9
choijinhap <wlsgkq123@gmail.com>
2022-04-07 21:40:02 +0900

    dev : Adding SignIn component temporary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigaion/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 const Shop = () => {
 	return <h1>I am the shop page</h1>;
 };
@@ -10,6 +11,7 @@ const App = () => {
 			<Route path='/' element={<Navigation />}>
 				<Route index element={<Home />}></Route>
 				<Route path='shop' element={<Shop />}></Route>
+				<Route path='sign-in' element={<SignIn />}></Route>
 			</Route>
 		</Routes>
 	);
