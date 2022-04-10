4bacb29c90c7063db74977a22eaf1525284963bb
choijinhap <wlsgkq123@gmail.com>
2022-04-10 18:14:31 +0900

    장바구니 담기 구현

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,19 @@
-import Button from '../button/button.component'
-import './cart-dropdown.style.scss'
+import { useContext } from 'react';
+import Button from '../button/button.component';
+import { CartContext } from '../../contexts/cart.context';
 
-const CartDropdown=() => {
-  return <div className='cart-dropdown-container'>
-      <div className="cart-items"></div>
-      <Button>Go To Checkout</Button>
-  </div>
-}
-export default CartDropdown
+import './cart-dropdown.style.scss';
+import CartItem from '../cart-item/cart-item.component';
+
+const CartDropdown = () => {
+    const {cartItems} = useContext(CartContext);
+	return (
+		<div className='cart-dropdown-container'>
+			<div className='cart-items'>
+                {cartItems.map(cartItem=>(<CartItem key={cartItem.id} cartItem={cartItem}/>))}
+            </div>
+			<Button>Go To Checkout</Button>
+		</div>
+	);
+};
+export default CartDropdown;
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,7 +5,7 @@ import { CartContext } from '../../contexts/cart.context';
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
-	const { setIsCartOpen } = useContext(CartContext);
+	const { setIsCartOpen, cartItems } = useContext(CartContext);
 	const toggleIsCartOpen = () => {
 		setIsCartOpen((cur) => !cur);
 	};
@@ -13,7 +13,7 @@ const CartIcon = () => {
 	return (
 		<div className='cart-icon-container' onClick={toggleIsCartOpen}>
 			<ShoppingIcon className='shopping-icon' />
-			<span className='item-count'>0</span>
+			<span className='item-count'>{cartItems.length}</span>
 		</div>
 	);
 };
