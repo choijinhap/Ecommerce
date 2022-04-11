6aaf1d87f44b3be4c2e96661efd846832d868d39
choijinhap <wlsgkq123@gmail.com>
2022-04-11 17:38:59 +0900

    Nested Router 사용하여 shop의 카테고리별 라우팅

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,12 +1,16 @@
+import { useNavigate } from 'react-router-dom';
 import ProductCard from '../product-card/product-card.component';
 
 import './category-preview.styles.scss';
 
 const CategoryPreview = ({ title, products }) => {
+	const navigate = useNavigate();
 	return (
 		<div className='category-preview-container'>
 			<h2>
-				<span className='title'>{title.toUpperCase()}</span>
+				<span className='title' onClick={()=>navigate(title)}>
+					{title.toUpperCase()}
+				</span>
 			</h2>
 			<div className='preview'>
 				{products
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,22 +1,14 @@
-import { useContext, Fragment } from 'react';
-
-import { CategoriesContext } from '../../contexts/categories.context';
-import CategoryPreview from '../../components/category-preview/category-preview.component';
-
+import { Route, Routes } from 'react-router-dom';
+import CatergoriesPreview from '../../components/categories-preview/cate/categories-preview.component';
+import Category from '../category/category.component';
 import './shop.styles.scss';
 
 const Shop = () => {
-	const { categoriesMap } = useContext(CategoriesContext);
 	return (
-		<div className='shop-container'>
-			{Object.keys(categoriesMap).map((title) => (
-				<CategoryPreview
-					key={title}
-					title={title}
-					products={categoriesMap[title]}
-				/>
-			))}
-		</div>
+		<Routes>
+			<Route index element={<CatergoriesPreview />} />
+			<Route path=':category' element={<Category />} />
+		</Routes>
 	);
 };
 
