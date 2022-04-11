32a69a952393f17c2a5bdce550f61bbafac10243
choijinhap <wlsgkq123@gmail.com>
2022-04-11 15:13:21 +0900

    옷 데이터 파이어베이스에 저장 및 불러오기 구현 CategoriesContext

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,29 @@
+import { createContext, useState, useEffect } from 'react';
+
+import { addCollectionAndDocuments,getCollectionAndDocuments } from '../utils/firebase/firebase.utils.js';
+
+import SHOP_DATA from '../shop-data.js';
+
+export const CategoriesContext = createContext({
+	categoriesMap: {},
+});
+
+export const CategoriesProvider = ({ children }) => {
+	const [categoriesMap, setCategoriesMap] = useState({});
+	// useEffect(() => {
+	//   addCollectionAndDocuments('categories',SHOP_DATA);
+	// },[])
+	useEffect(() => {
+		const getCategories=async () => {
+			const categoryMap=await getCollectionAndDocuments()
+			console.log(categoryMap)
+			setCategoriesMap(categoryMap);
+		}
+		getCategories();
+	},[])
+
+	const value = { categoriesMap };
+	return (
+		<CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>
+	);
+};
