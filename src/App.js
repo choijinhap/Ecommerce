aeced5bc3702dd2c6e4c8c3f0f4c4a513cb2cfd2
choijinhap <wlsgkq123@gmail.com>
2022-04-15 01:39:21 +0900

    userContext를 리덕스로 마이그레이션

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { createUserDocumentFromAuth, onAuthStateChangedListener } from './utils/firebase/firebase.utils';
+
+import { setCurrentUser } from './store/user/user.action';
+
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigaion/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
@@ -6,6 +12,18 @@ import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
 
 const App = () => {
+	const dispatch=useDispatch();
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChangedListener((user) => {
+			if (user) {
+				createUserDocumentFromAuth(user);
+			}
+			dispatch(setCurrentUser(user));
+		});
+		return unsubscribe;
+	}, []);
+
 	return (
 		<Routes>
 			<Route path='/' element={<Navigation />}>
--- src/routes/navigaion/navigation.component.jsx
+++ src/routes/navigaion/navigation.component.jsx
@@ -1,12 +1,13 @@
 import { Fragment, useContext } from 'react';
 import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import { CartContext } from '../../contexts/cart.context';
-import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+import { selectCurrentUser } from '../../store/user/user.selector';
 
 import {
 	NavigationContainer,
@@ -16,7 +17,7 @@ import {
 } from './naviagtion.styles.jsx';
 
 const Navigation = () => {
-	const { currentUser } = useContext(UserContext);
+	const currentUser = useSelector(selectCurrentUser);
 	const { isCartOpen } = useContext(CartContext);
 
 	return (
