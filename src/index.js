2b76015e7184fc09ee61af61058e80dfe50cb538
choijinhap <wlsgkq123@gmail.com>
2022-04-17 11:38:51 +0900

    redux-persist 적용

--- src/index.js
+++ src/index.js
@@ -5,16 +5,19 @@ import { Provider } from 'react-redux';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from './store/store';
+import { store,persistor} from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.scss';
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<BrowserRouter>
-					<App />
-			</BrowserRouter>
+			<PersistGate persistor={persistor}>
+				<BrowserRouter>
+						<App />
+				</BrowserRouter>
+			</PersistGate>
 		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
