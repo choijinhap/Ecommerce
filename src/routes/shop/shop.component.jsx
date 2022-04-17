e2df793ba6bba30f9f26f610739c297d9d38be90
choijinhap <wlsgkq123@gmail.com>
2022-04-17 22:46:20 +0900

    redux thunk 이용하여 categories 비동기로 dispatch하기

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,19 +3,14 @@ import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 
 import CatergoriesPreview from '../../components/categories-preview/cate/categories-preview.component';
-import { setCategories } from '../../store/category/category.action';
-import { getCollectionAndDocuments } from '../../utils/firebase/firebase.utils';
+import { fetchCategoriesAsync } from '../../store/category/category.action';
 import Category from '../category/category.component';
 import './shop.styles.scss';
 
 const Shop = () => {
 	const dispatch = useDispatch();
 	useEffect(() => {
-		const getCategories = async () => {
-			const categoriesArray = await getCollectionAndDocuments();
-			dispatch(setCategories(categoriesArray));
-		};
-		getCategories();
+		dispatch(fetchCategoriesAsync());
 	}, []);
 	return (
 		<Routes>
--- src/store/category/category.reducer.js
+++ src/store/category/category.reducer.js
@@ -2,13 +2,19 @@ import { CATEGORIES_ACTION_TYPES } from './category.type';
 
 export const CATEGORIES_INITIAL_STATE = {
 	categories: [],
+	isLoading: false,
+	error: null,
 };
 
 export const categoriesRedcuer = (state = CATEGORIES_INITIAL_STATE, action) => {
 	const { type, payload } = action;
 	switch (type) {
-		case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
-			return { ...state, categories: payload };
+		case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
+			return { ...state, categories: payload, lsLoading: false };
+		case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
+			return { ...state, isLoading: true };
+		case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
+			return { ...state, error: payload, isLoading: false };
 		default:
 			return state;
 	}
