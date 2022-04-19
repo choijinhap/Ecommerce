c443e8f2b22903fb58aaae662c2b31bca1ffc9a8
choijinhap <wlsgkq123@gmail.com>
2022-04-19 22:19:50 +0900

    fix createUserDocumentFromAuth

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -87,7 +87,7 @@ export const createUserDocumentFromAuth = async (
 			console.log('error creating the user', err.message);
 		}
 	}
-	return userDocRef;
+	return userSnapshot;
 };
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
